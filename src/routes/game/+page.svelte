2e206b3cdfc990b4806e22dd23c5395cafd72a1a
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-07-14 19:35:14 -0300

    Create main game containers.

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
+	import Root from "$lib/Root.svelte";
 	import TopContainer from "$lib/TopContainer.svelte";
 	import TopInfo from "$lib/TopInfo.svelte";
+	import Footer from "$lib/Footer.svelte";
+	import MainPane from "$lib/MainPane.svelte";
+	import Kana from "$lib/Kana.svelte";
+	import Separator from "$lib/Separator.svelte";
+	import RomajiBar from "$lib/RomajiBar.svelte";
 </script>
 
-<div>
+<Root>
 	<TopContainer>
 		<TopInfo />
 	</TopContainer>
-</div>
+
+	<MainPane>
+		<Kana />
+		<Separator />
+		<RomajiBar />
+	</MainPane>
+	<Footer />
+</Root>
 
 <style>
 </style>
