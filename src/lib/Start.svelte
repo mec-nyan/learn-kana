62f6821a1f75dabef9e41c915730e2a9c27c8bba
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-07-14 19:06:08 -0300

    Expand "start" button clickable area.

--- src/lib/Start.svelte
+++ src/lib/Start.svelte
@@ -5,18 +5,20 @@
 
 <style>
 	div {
+		width: 60%;
+	}
+	a {
 		background: hsl(243, 20%, 18%);
 		border: 1px solid hsl(243, 10%, 30%);
 		font-family: "Hack", monospace;
 		font-size: 1.25rem;
-		width: 60%;
-		text-align: center;
-		padding: 1rem;
+		text-decoration: none;
 		border-radius: 8px;
 		cursor: pointer;
-	}
-	a {
-		text-decoration: none;
 		color: inherit;
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
 	}
 </style>
