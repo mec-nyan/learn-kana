3e69c9bf78bac451cc604e9bd36b1f0ccf9033ec
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-07-15 19:22:01 -0300

    Add kana map.

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -7,6 +7,108 @@
 	import Kana from "$lib/Kana.svelte";
 	import Separator from "$lib/Separator.svelte";
 	import RomajiBar from "$lib/RomajiBar.svelte";
+
+	// Hiragana chart.
+	const kana_map = [
+		{
+			name: "a",
+			kanas: [
+				{ romaji: "a", hiragana: "あ" },
+				{ romaji: "i", hiragana: "い" },
+				{ romaji: "u", hiragana: "う" },
+				{ romaji: "e", hiragana: "え" },
+				{ romaji: "o", hiragana: "お" },
+			],
+		},
+		{
+			name: "ka",
+			kanas: [
+				{ romaji: "ka", hiragana: "か" },
+				{ romaji: "ki", hiragana: "き" },
+				{ romaji: "ku", hiragana: "く" },
+				{ romaji: "ke", hiragana: "け" },
+				{ romaji: "ko", hiragana: "こ" },
+			],
+		},
+		{
+			name: "sa",
+			kanas: [
+				{ romaji: "sa", hiragana: "さ" },
+				{ romaji: "shi", hiragana: "し" },
+				{ romaji: "su", hiragana: "す" },
+				{ romaji: "se", hiragana: "せ" },
+				{ romaji: "so", hiragana: "そ" },
+			],
+		},
+		{
+			name: "ta",
+			kanas: [
+				{ romaji: "ta", hiragana: "た" },
+				{ romaji: "chi", hiragana: "ち" },
+				{ romaji: "tsu", hiragana: "つ" },
+				{ romaji: "te", hiragana: "て" },
+				{ romaji: "to", hiragana: "と" },
+			],
+		},
+		{
+			name: "na",
+			kanas: [
+				{ romaji: "na", hiragana: "な" },
+				{ romaji: "ni", hiragana: "に" },
+				{ romaji: "nu", hiragana: "ぬ" },
+				{ romaji: "ne", hiragana: "ね" },
+				{ romaji: "no", hiragana: "の" },
+			],
+		},
+		{
+			name: "ha",
+			kanas: [
+				{ romaji: "ha", hiragana: "は" },
+				{ romaji: "hi", hiragana: "ひ" },
+				{ romaji: "fu", hiragana: "ふ" },
+				{ romaji: "he", hiragana: "へ" },
+				{ romaji: "ho", hiragana: "ほ" },
+			],
+		},
+		{
+			name: "ma",
+			kanas: [
+				{ romaji: "ma", hiragana: "ま" },
+				{ romaji: "mi", hiragana: "み" },
+				{ romaji: "mu", hiragana: "む" },
+				{ romaji: "me", hiragana: "め" },
+				{ romaji: "mo", hiragana: "も" },
+			],
+		},
+		{
+			name: "ya",
+			kanas: [
+				{ romaji: "ya", hiragana: "や" },
+				{ romaji: "yu", hiragana: "ゆ" },
+				{ romaji: "yo", hiragana: "よ" },
+			],
+		},
+		{
+			name: "ra",
+			kanas: [
+				{ romaji: "ra", hiragana: "ら" },
+				{ romaji: "ri", hiragana: "り" },
+				{ romaji: "ru", hiragana: "る" },
+				{ romaji: "re", hiragana: "れ" },
+				{ romaji: "ro", hiragana: "ろ" },
+			],
+		},
+		{
+			name: "wa",
+			kanas: [
+				{ romaji: "wa", hiragana: "わ" },
+				{ romaji: "i", hiragana: "ゐ" },
+				{ romaji: "n", hiragana: "ん" },
+				{ romaji: "e", hiragana: "ゑ" },
+				{ romaji: "wo", hiragana: "を" },
+			],
+		},
+	];
 </script>
 
 <Root>
@@ -15,9 +117,9 @@
 	</TopContainer>
 
 	<MainPane>
-		<Kana />
+		<Kana kana={kana_map[1].kanas[0].hiragana}/>
 		<Separator />
-		<RomajiBar />
+		<RomajiBar buttons={kana_map[1].kanas}/>
 	</MainPane>
 	<Footer />
 </Root>
